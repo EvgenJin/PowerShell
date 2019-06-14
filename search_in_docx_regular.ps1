12a057a842a69d0ad31697b0d63208bcb2478727
John Doe <email@email.com>
2019-06-14 12:33:28 +0500

    1
    1

---
+++ search_in_docx_regular.ps1
@@ -0,0 +1,11 @@
+﻿$Word = New-Object -ComObject Word.Application
+$Document = $Word.Documents.Open('Q:\BankXXI\TEMPLATE.WORD\kred_dog\2014\Ипотека\ЗАКЛАДНЫЕ\20.04.2017\ЗАКЛАДНАЯ_СОЗАЕМ1_2_3_КВАРТ.dot')
+$range = $Document.content
+
+$Document.Paragraphs | ForEach-Object {
+    $string = $_.Range.Text
+    ($string| select-string "{\D+?}" -allmatches).Matches.Value
+}
+
+$Word.Documents.Close($false)
+
