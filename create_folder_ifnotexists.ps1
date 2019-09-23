8bc0ac5a8f5fb552b119fda88a94d9661c753443
EvgenJin <rumbiroid@gmail.com>
2019-09-23 14:10:49 +0500

    1
    1

---
+++ create_folder_ifnotexists.ps1
@@ -0,0 +1,12 @@
+﻿$csv = Import-Csv "C:\tmp\process.csv"
+
+foreach($item in $csv)
+    {
+        #$($item.login)
+        if(!(Test-Path T:\$($item.login) -PathType Container)) { 
+            New-Item -ItemType directory -Path T:\$($item.login)
+            Write-Host T:\$($item.login) + 'created'
+        } else {
+            write-host $($item.login)+"-- Path found"
+        }
+    }
---
+++ searchInLogs.ps1
@@ -0,0 +1,15 @@
+﻿Do {
+  # Write-Host "Где искать?  => " -ForegroundColor Green -NoNewline
+  $folder = '\\10.26.232.39\log4j'
+  Write-Host "Что искать?  => " -ForegroundColor Green -NoNewline
+  # $folder = 'C:/tmp/logs'
+  $a = Get-ChildItem -Path $folder\*log -recurse 
+  $search = Read-Host
+  foreach ($item in $a) {
+    $res = Get-Content $item.FullName -Encoding UTF8 | Select-String $search
+    if ($res){
+      Write-Host $item.FullName' --> '$res
+    } 
+  }
+}
+while ($true)
