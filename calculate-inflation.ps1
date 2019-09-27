196e6e09e4cf42c838ac86b8eb21dce6cc0be52f
EvgenJin <rumbiroid@gmail.com>
2019-09-27 12:09:41 +0500

    1
    1

---
+++ calculate-inflation.ps1
@@ -0,0 +1,8 @@
+﻿$Start = '2014/12/15'
+$end = '2019/09/24'
+$amount = 36800
+$type = 'price' #value
+# price - изменение цены value - изменение стоимости денег
+$request =  'https://www.statbureau.org/calculate-inflation-'+$type+'-json?country=russia&start='+$Start+'&end='+$end+'&amount='+$amount+'&format=false'
+Invoke-WebRequest $request |
+ConvertFrom-Json
