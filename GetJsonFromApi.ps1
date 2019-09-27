7874983994ce554b6242292349383f3c2f36e75f
EvgenJin <rumbiroid@gmail.com>
2019-09-27 12:02:34 +0500

    1
    1

---
+++ GetJsonFromApi.ps1
@@ -0,0 +1,6 @@
+﻿$search = 'Екатеринбург, Ирбитская 11'
+$request =  'https://kladr-api.ru/api.php?query='+$search+'&oneString=1&limit=50&withParent=0'
+Invoke-WebRequest $request |
+ConvertFrom-Json |
+select -expand result |
+Select id, name, zip, okato, fullName, oktmo, parentGuid
---
+++ GetKladrFiasByAddress.ps1
@@ -0,0 +1,7 @@
+﻿$search = 'Екатеринбург, Ирбитская 11'
+$request =  'https://kladr-api.ru/api.php?query='+$search+'&oneString=1&limit=50&withParent=1'
+Invoke-WebRequest $request |
+ConvertFrom-Json |
+select -expand result |
+Select id, name, zip, okato, fullName, oktmo, parentGuid
+# https://devblogs.microsoft.com/scripting/playing-with-json-and-powershell/
